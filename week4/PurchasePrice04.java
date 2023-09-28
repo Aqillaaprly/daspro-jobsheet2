d418f408a5bd1e46e0eadd3322fa656a92b67415
Aqillaprly <aqillaaprly.gmail.com>
2023-09-28 15:13:42 +0700

    for jobsheet

--- week4/PurchasePrice04.java
+++ week4/PurchasePrice04.java
@@ -3,20 +3,29 @@ import java.util.Scanner;
 public class PurchasePrice04 {
     public static void main(String[] args) {
     Scanner input =new Scanner(System.in);
-    int price, quantity;
-    double discount=0.1, totalPrice, purchasePrice, totalDiscount;
+    String bookBrand;
+    int price, quantity, pageCount;
+    double totalPrice, purchasePrice, totalDiscount, discount;
 
+    System.out.println("Input Book Brand");
+    bookBrand=input.nextLine();
     System.out.println("Input price: ");
     price=input.nextInt();
     System.out.println("Input quantity: ");
     quantity=input.nextInt();
+    System.out.println("Input Page Count");
+    pageCount=input.nextInt();
+    System.out.println("Input Discount");
+    discount=input.nextInt();
 
     totalPrice=price*quantity;
     totalDiscount=totalPrice*discount;
     purchasePrice=totalPrice-totalDiscount;
 
-    System.out.println("Total discount: "+totalDiscount);
-    System.out.println("Final purchase price: "+purchasePrice);
+    System.out.println("Book Brand: " +bookBrand);
+    System.out.println("Book Page: " +pageCount);
+    System.out.println("Total discount: " +totalDiscount);
+    System.out.println("Final purchase price: " +purchasePrice);
 
     }
 }
--- week4/Salary04.java
+++ week4/Salary04.java
@@ -5,15 +5,18 @@ public class Salary04{
     public static void main(String[] args) {
     Scanner input =new Scanner(System.in);
 
-    int numAttendence, numAbsence, totalSalary;
-    int salary=40000, salaryDeduction=25000;
+    int numAttendence, numAbsence, totalSalary, salary, salaryDeduction;
 
     System.out.println("Input attendence number: ");
     numAttendence = input.nextInt();
     System.out.println("Input absence number: ");
     numAbsence = input.nextInt();
+    System.out.println("Input salary");
+    salary = input.nextInt();
+    System.out.println("Input salary deduction");
+    salaryDeduction = input.nextInt();
 
-    totalSalary=(numAttendence*salary)-(numAbsence*salaryDeduction);
+    totalSalary=(numAttendence*salary)-(numAbsence*salary)-(salaryDeduction);
     System.out.println("Total salary:"+totalSalary);
 
     }
