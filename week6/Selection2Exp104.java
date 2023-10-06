f5e0d08b366c1e280eb7699f489fed3019f1c773
Aqillaprly <aqillaaprly.gmail.com>
2023-10-06 14:08:30 +0700

    jobsheet need

--- week6/Selection2Exp104.java
+++ week6/Selection2Exp104.java
@@ -10,8 +10,11 @@ public class Selection2Exp104 {
     year = input04.nextInt();
 
     if(year %4 == 0)
-        if(year %100 != 0) 
+        if(year %100 == 0 && year %400 == 0){
          System.out.println("Leap year"); 
+         }else 
+          System.out.println("Not leap year");
+          
     else 
         System.out.println("Not leap year");
     }
