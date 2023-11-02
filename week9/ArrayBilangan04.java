378dd961dc40ec24deb362c7cc907ebf49094e62
Aqillaprly <aqillaaprly.gmail.com>
2023-11-02 17:29:33 +0700

    1st question

--- week9/ArrayBilangan04.java
+++ week9/ArrayBilangan04.java
@@ -4,10 +4,10 @@ public class ArrayBilangan04 {
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int [] bil = new int[4];
-    bil[0] = 5;
-    bil[1] = 13;
-    bil[2] = -7;
-    bil[3] = 17;
+    bil[0] = (int) 5.0;
+    bil[1] = 12867;
+    bil[2] = (int) 7.5;
+    bil[3] = 2000000;
 
     System.out.println(bil[0]);
     System.out.println(bil[1]);
