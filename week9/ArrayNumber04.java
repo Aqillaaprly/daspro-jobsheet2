a83e6659e753efd6543083a4b5d38c158c3d6af9
Aqillaprly <aqillaaprly.gmail.com>
2023-11-09 21:30:55 +0700

    INISIALITATION

--- week9/ArrayNumber04.java
+++ week9/ArrayNumber04.java
@@ -4,11 +4,7 @@ public class ArrayNumber04 {
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    int[] number = new int[4];
-    number [0] = (int)5.0;
-    number [1] = 12;
-    number [2] = (int)7.5;
-    number [3] = 20;
+    int[] number = {5,12,7,20};
 
     System.out.println(number[0]);
     System.out.println(number[1]);
